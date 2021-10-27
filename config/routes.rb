21dfd662789bfe6f7dca366aae4a2607b7f52933
David Boureau <bdavidxyz@gmail.com>
2021-10-27 21:15:59 +0200

    signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do  
   get "welcome/index"  
   resources 'login', only: [:index, :create]
+  resources 'signup', only: [:index, :create]
   root to: "welcome#index"  
 end  
